27fb525d524e59ce02371a361bec55d80dc03783
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-02-05 14:49:25 +0530

    adding the sidebar function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Sidebar/>
     </div>
   );
 }
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,11 @@
+import '../assets/css/style.css'
+
+const Sidebar=()=>{
+    return(
+        <>
+            <h1>Sidebar</h1>
+        </>
+    )
+};
+
+export default Sidebar;
