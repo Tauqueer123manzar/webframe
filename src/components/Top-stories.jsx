4970233f8e271136c6d85bd7a68b3bda66e76adf
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-02-05 15:54:23 +0530

    changing and merg topstories

--- src/components/Top-stories.jsx
+++ src/components/Top-stories.jsx
@@ -1,7 +1,41 @@
+import '../assets/css/style.css'
+import image1  from '../assets/images/image1.jpg'
+
 const TopStoriesCard=()=>{
     return(
-        <div className="col-4">
-            hello
+        <div className="col-4 card">
+            <div className="container">
+
+                <div className="card-1">
+                    <div className="img">
+                        <img src={image1} alt="image" />
+                    </div>
+
+                    <div className="top">
+                        <div className="view">
+                            <i class="fa-regular fa-eye"></i>
+                        </div>
+                        <div className="analytics">
+                            <i class="fa-sharp fa-solid fa-file-chart-pie"></i>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        <p>How 7 lines code turned into <span>$36 Billion Empire</span></p>
+                    </div>
+
+                    <div className="business">
+                        <h1>BUSINESS.</h1>
+                        <p>20 sep</p>
+                        <button class="btn-1">Published</button>
+                    </div>
+
+                    <div className="buttom">
+                        <button class="btn-2">view</button>
+                        <button class="btn-3"><i class="fa-regular fa-ellipsis"></i></button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
